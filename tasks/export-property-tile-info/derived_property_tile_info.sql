02c560fe0b7a85e10c41237d6753c20a37c04060
LingxuanGao <geo459945@gmail.com>
2026-04-08 15:46:48 -0400

    debugging
    -waiting for #12 ML model
    -change name

--- tasks/export-property-tile-info/derived_property_tile_info.sql
+++ tasks/export-property-tile-info/derived_property_tile_info.sql
@@ -14,15 +14,13 @@ CREATE OR REPLACE TABLE `derived.property_tile_info` AS (
         o.property_id,
         o.location AS address,
         par.geometry AS geog,
-        ca.assessed_value AS current_assessed_value,
+        NULL AS current_assessed_value,
         la.tax_year_assessed_value
     FROM `core.opa_properties` AS o
     JOIN `core.pwd_parcels` AS par
         ON o.property_id = par.property_id
     LEFT JOIN latest_assessments AS la
         ON o.property_id = la.property_id
-    LEFT JOIN `derived.current_assessments` AS ca
-        ON o.property_id = ca.property_id
     WHERE o.category_code_description = 'RESIDENTIAL'
         AND par.geometry IS NOT NULL
 );
