353fc925ca4223bf872210f19f15ced4ea66e117
LingxuanGao <geo459945@gmail.com>
2026-04-23 06:48:03 -0400

    Update core_phl_opa_assessments.sql

--- tasks/load-opa-assessments/core_phl_opa_assessments.sql
+++ tasks/load-opa-assessments/core_phl_opa_assessments.sql
@@ -5,11 +5,11 @@ CREATE OR REPLACE TABLE `core.opa_assessments` AS (
 
         SAFE_CAST(year AS INT64) AS year,
 
-        SAFE_CAST(market_value     AS NUMERIC) AS market_value,
-        SAFE_CAST(taxable_land     AS NUMERIC) AS taxable_land,
+        SAFE_CAST(market_value AS NUMERIC) AS market_value,
+        SAFE_CAST(taxable_land AS NUMERIC) AS taxable_land,
         SAFE_CAST(taxable_building AS NUMERIC) AS taxable_building,
-        SAFE_CAST(exempt_land      AS NUMERIC) AS exempt_land,
-        SAFE_CAST(exempt_building  AS NUMERIC) AS exempt_building,
+        SAFE_CAST(exempt_land AS NUMERIC) AS exempt_land,
+        SAFE_CAST(exempt_building AS NUMERIC) AS exempt_building,
 
         * EXCEPT (
             parcel_number,
